10beda82504387c5c61b2973482c1834a9c57380
BenedettaGiarda <bene.g0799@gmail.com>
2023-03-30 16:04:33 +0200

    estetica Grafico

--- Prove/View/Grafico.java
+++ Prove/View/Grafico.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 
 public class Grafico extends JPanel{
 
-    public Grafico() throws IOException {
+    public Grafico() {
         JFrame frame1 = new JFrame();
         frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JFrame frame = new JFrame();
@@ -35,73 +35,84 @@ public class Grafico extends JPanel{
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // Visualizza la finestra di dialogo per selezionare un file
         int result = fileChooser.showOpenDialog(frame);
-        File file = fileChooser.getSelectedFile();
-        HashMap<Float, Date> risultati = ReadCSVFile.readFile(file);
-        // Creazione della serie di dati
-        XYSeries serie = new XYSeries("Dati di esempio");
-        Date primaData=null;
-        long DT=0;
-        for (Float key : risultati.keySet()) {
-            if(primaData==null){
-                primaData=risultati.get(key);
-                DT = primaData.getTime();
+
+        //controllo che effettivamente sia stato selezionato un file
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+
+            HashMap<Float, Date> risultati = ReadCSVFile.readFile(file);
+            // Creazione della serie di dati
+            XYSeries serie = new XYSeries("Dati di esempio");
+            Date primaData=null;
+            long DT=0;
+            for (Float key : risultati.keySet()) {
+                if(primaData==null){
+                    primaData=risultati.get(key);
+                    DT = primaData.getTime();
+                }
+                if(risultati.get(key).getTime()-DT<0){
+                    DT=risultati.get(key).getTime();
+                }
             }
-            if(risultati.get(key).getTime()-DT<0){
-                DT=risultati.get(key).getTime();
+
+            for (Float key : risultati.keySet()) {
+
+                Date value = risultati.get(key);
+                long timeinMillis = value.getTime()-DT;
+
+                serie.add(timeinMillis/1000, key);
             }
+
+            // Creazione della collezione di serie
+            XYSeriesCollection dataset = new XYSeriesCollection();
+            dataset.addSeries(serie);
+
+            // Creazione del grafico
+            JFreeChart chart = ChartFactory.createXYLineChart(
+                    "Grafico a linee", // Titolo del grafico
+                    "X", // Etichetta asse X
+                    "Y", // Etichetta asse Y
+                    dataset, // Collezione di serie
+                    PlotOrientation.VERTICAL, // Orientamento del grafico
+                    true, // Mostra legenda
+                    true, // Mostra tooltip
+                    false // Mostra URL
+            );
+            NumberAxis yAxis = (NumberAxis) chart.getXYPlot().getRangeAxis();
+            yAxis.setAutoRange(true);
+
+            NumberAxis xAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
+            xAxis.setAutoRange(true);
+
+            // Personalizzazione del grafico
+            chart.setBackgroundPaint(Color.white);
+            XYPlot plot = chart.getXYPlot();
+            plot.setBackgroundPaint(Color.lightGray);
+            plot.setDomainGridlinePaint(Color.white);
+            plot.setRangeGridlinePaint(Color.white);
+
+
+            JPanel grafico = new JPanel();
+            ChartPanel chartPanel = new ChartPanel(chart);
+            chartPanel.setPreferredSize(new Dimension(700,700));
+            JScrollPane scrollPane = new JScrollPane(chartPanel);
+            scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+            scrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
+            scrollPane.getHorizontalScrollBar().setUnitIncrement(10);
+            scrollPane.getVerticalScrollBar().setUnitIncrement(10);
+            scrollPane.setPreferredSize(new Dimension(700,700));
+            grafico.add(scrollPane);
+            frame1.add(grafico);
+            frame1.setSize(new Dimension(700,700));
+            frame1.setVisible(true);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Nessun file selezionato", "Errore", JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
         }
 
-        for (Float key : risultati.keySet()) {
 
-            Date value = risultati.get(key);
-            long timeinMillis = value.getTime()-DT;
-
-            serie.add(timeinMillis/1000, key);
-        }
-
-        // Creazione della collezione di serie
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        dataset.addSeries(serie);
-
-        // Creazione del grafico
-        JFreeChart chart = ChartFactory.createXYLineChart(
-                "Grafico a linee", // Titolo del grafico
-                "X", // Etichetta asse X
-                "Y", // Etichetta asse Y
-                dataset, // Collezione di serie
-                PlotOrientation.VERTICAL, // Orientamento del grafico
-                true, // Mostra legenda
-                true, // Mostra tooltip
-                false // Mostra URL
-        );
-        NumberAxis yAxis = (NumberAxis) chart.getXYPlot().getRangeAxis();
-        yAxis.setAutoRange(true);
-
-        NumberAxis xAxis = (NumberAxis) chart.getXYPlot().getDomainAxis();
-        xAxis.setAutoRange(true);
-
-        // Personalizzazione del grafico
-        chart.setBackgroundPaint(Color.white);
-        XYPlot plot = chart.getXYPlot();
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
-
-
-        JPanel grafico = new JPanel();
-        ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new Dimension(700,700));
-        JScrollPane scrollPane = new JScrollPane(chartPanel);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        scrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
-        scrollPane.getHorizontalScrollBar().setUnitIncrement(10);
-        scrollPane.getVerticalScrollBar().setUnitIncrement(10);
-        scrollPane.setPreferredSize(new Dimension(700,700));
-        grafico.add(scrollPane);
-        frame1.add(grafico);
-        frame1.setSize(new Dimension(700,700));
-        frame1.setVisible(true);
     }
 
 
